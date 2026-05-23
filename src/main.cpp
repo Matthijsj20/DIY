7b570d324a35627b7e6d382354237bd3bc6a6c32
Matthijs Jacobs <matthijs.jacobs@bigassbattery.com>
2026-05-23 18:22:25 +0200

    client code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
 #include <pins.hpp>
 
 void toggleLED() {
@@ -7,14 +9,30 @@ void toggleLED() {
   Serial.println("LED TOGGLED to " + String(!currentState));
 }
 
-// Built-in LED on most NodeMCU-32S boards
+void onDataRecv(const uint8_t *mac, const uint8_t *data, int len) {
+  (void)mac;
+  (void)data;
+  (void)len;
+  toggleLED();
+}
+
 void setup() {
   pinMode(INTERNAL_LED_PIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("BOOT");
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("ESP-NOW init failed");
+    return;
+  }
+
+  esp_now_register_recv_cb(onDataRecv);
+
+  Serial.print("ESP-NOW client ready. MAC: ");
+  Serial.println(WiFi.macAddress());
 }
 
 void loop() {
-  toggleLED();
-  delay(1000);
 }
